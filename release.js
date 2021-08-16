3f5087a2054d7032912ec7e7a2dc0d56039d97f8
rondymesquita <rondinellimesquitas@gmail.com>
2021-08-15 22:24:44 -0300

    fix(SCOPE-1) ON GOING

--- release.js
+++ release.js
@@ -1,5 +1,22 @@
 const conventionalcommits = require("conventional-changelog-conventionalcommits");
 const conventionalChangelog = require("conventional-changelog");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
+// async function main() {
+//   const { stdout: lastestVersion } = await exec(
+//     "git describe --tags --abbrev=0"
+//   );
+//   const { stdout: changelog } = await exec(
+//     `git log ${lastestVersion}..HEAD --oneline`
+//   );
+//   console.log(changelog);
+// }
+// main();
+
+const [from, to] = process.argv.slice(2);
+
+console.log(process.argv.slice(2));
 
 const config = conventionalcommits({
   issuePrefixes: ["TEST-"],
@@ -19,14 +36,32 @@ const config = conventionalcommits({
 config.then((c) => {
   conventionalChangelog(
     {
-      releaseCount: 5,
+      // pkg: {
+      //   transform: function (commit) {
+      //     console.log(arguments);
+      //   },
+      // },
+      releaseCount: 2,
       config: c,
     },
     null,
+    // {
+    //   version: to,
+    // },
     null,
+    // { from: from },
     null,
     {
       groupBy: "scope",
+      // transform: function (commit, cb) {
+      //   console.log(commit);
+      //   // return;
+      //   // cb();
+      // },
+      // finalizeContext(context, options, commits, keyCommit) {
+      //   // console.log(commits);
+      //   // return { commitGroups: commits };
+      // },
     }
   ).pipe(process.stdout);
 });
