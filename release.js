5fdbbc335fbaed1053714c65ca1e108a97b9989b
rondymesquita <rondinellimesquitas@gmail.com>
2021-08-15 21:03:14 -0300

    feat(SCOPE-2) feat5

--- release.js
+++ release.js
@@ -1,86 +1,32 @@
-const release = require("release-it");
+const conventionalcommits = require("conventional-changelog-conventionalcommits");
 const conventionalChangelog = require("conventional-changelog");
-const conventionalcommitsConfig = require("conventional-changelog-conventionalcommits");
-const fs = require("fs");
-const options = fs.readFileSync(".release-it.json").toString();
 
-// const options = conventionalcommitsConfig({
-//   types: [
-//     { type: "feat", section: "Features" },
-//     { type: "fix", section: "Bug Fixes" },
-//     { type: "chore", section: "Chores" },
-//     { type: "docs", section: "Docos" },
-//     { type: "style", section: "Code Style" },
-//     { type: "refactor", section: "Refactors" },
-//     { type: "perf", section: "Performance" },
-//     { type: "test", section: "Tests" },
-//   ],
-//   // issuePrefixes: ["TEST-"],
-//   // issueUrlFormat: "myBugTracker.com/{prefix}{id}",
-// }).then(function (config) {
-//   // console.log(Object.keys(config));
-// });
+const config = conventionalcommits({
+  issuePrefixes: ["TEST-"],
+  issueUrlFormat: "myBugTracker.com/{prefix}{id}",
+  types: [
+    { type: "feat", section: "Features" },
+    { type: "fix", section: "Bug Fixes" },
+    { type: "chore", section: "Chores" },
+    { type: "docs", section: "Docos" },
+    { type: "style", section: "Code Style" },
+    { type: "refactor", section: "Refactors" },
+    { type: "perf", section: "Performance" },
+    { type: "test", section: "Tests" },
+  ],
+});
 
-conventionalChangelog(
-  {
-    // config: () => conventionalcommitsConfig,
-    preset: "conventionalcommits",
-    // releaseCount: 2,
-    // transform: function(commit, cb) {
-    //   console.log('>>',commit, '<<')
-    //   // this.pipe(cb())
-    //   // cb()
-    //   commit = {
-    //     ...commit,
-    //     type: "rondy"
-    //   }
-    //   cb(commit);
-    // },
-  },
-  null,
-  {
-    from: 1,
-  },
-  null,
-  {
-    groupBy: "scope",
-    transform: function (commit) {
-      // console.log("commit", commit);
+config.then((c) => {
+  conventionalChangelog(
+    {
+      releaseCount: 5,
+      config: c,
     },
-    finalizeContext: function (ctx, opts, commits, keyCommit) {
-      console.log(ctx, opts, commits, keyCommit);
-    },
-  }
-).pipe(process.stdout);
-
-// console.log(options);
-// console.log("1");
-
-// conventionalChangelog({
-//   config: conventionalcommitsConfig,
-// }).pipe(process.stdout);
-
-// conventionalChangelog(
-//   {
-//     releaseCount: 3,
-//     preset: {},
-//     // transform: function (params, cb) {
-//     //   // console.log(params);
-//     //   cb(params);
-//     // },
-//   },
-//   null,
-//   null,
-//   null,
-//   {
-//     context: {},
-//     transform: function (commit, ctx) {
-//       // console.log(commit);
-//     },
-//   }
-// ).pipe(process.stdout);
-
-// release(options).then((output) => {
-//   console.log(output);
-
-// });
+    null,
+    null,
+    null,
+    {
+      groupBy: "scope",
+    }
+  ).pipe(process.stdout);
+});
