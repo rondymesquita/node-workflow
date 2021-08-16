1ad186a1f0c9a431e558320cd58f475c45d6b30a
rondymesquita <rondinellimesquitas@gmail.com>
2021-08-15 21:00:36 -0300

    chore(SCOPE-1) done

---
+++ release2.js
@@ -0,0 +1,34 @@
+const conventionalcommits = require("conventional-changelog-conventionalcommits");
+const conventionalChangelog = require("conventional-changelog");
+
+const config = conventionalcommits({
+  issuePrefixes: ["TEST-"],
+  issueUrlFormat: "myBugTracker.com/{prefix}{id}",
+  types: [
+    { type: "feat", section: "Features" },
+    { type: "fix", section: "Bug Fixes" },
+    { type: "chore", section: "Chores" },
+    { type: "docs", section: "Docos" },
+    { type: "style", section: "Code Style" },
+    { type: "refactor", section: "Refactors" },
+    { type: "perf", section: "Performance" },
+    { type: "test", section: "Tests" },
+  ],
+});
+
+config.then((c) => {
+  conventionalChangelog(
+    {
+      releaseCount: 4,
+      config: c,
+    },
+    null,
+    null,
+    null,
+    {
+      groupBy: "scope",
+    }
+  ).pipe(process.stdout);
+});
+
+// conventionalChangelog(c).pipe(process.stdout);
