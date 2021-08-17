2b4be8f5cd6eb2a0ecc50c3f693a8d6329666b5c
rondymesquita <rondinellimesquitas@gmail.com>
2021-08-17 17:24:19 -0300

    feat: json to js release

---
+++ .release-it.js
@@ -0,0 +1,56 @@
+module.exports = {
+  // "dry-run": true,
+  // "dryRun": true,
+  "plugins": {
+    "@release-it/conventional-changelog": {
+      // "ignoreRecommendedBump": true,
+      "context": {
+        // "version": "0.0.37",
+        "linkCompare": false
+      },
+      // "gitRawCommitsOpts": {
+      //   "merges": null
+      // },
+      // "parserOpts": {
+
+      // },
+      // "writerOpts": {
+      //   "groupBy": "scope"
+      // },
+      // "infile": "CHANGELOG.md",
+      "preset": {
+        "name": "conventionalcommits",
+        "types": [
+          { "type": "feat", "section": "Features" },
+          { "type": "fix", "section": "Bug Fixes" },
+          { "type": "chore", "section": "Chores" },
+          { "type": "docs", "section": "Docos" },
+          { "type": "style", "section": "Code Style" },
+          { "type": "refactor", "section": "Refactors" },
+          { "type": "perf", "section": "Performance" },
+          { "type": "test", "section": "Tests" }
+        ]
+      }
+    }
+  },
+  "git": {
+    // "changelog": "git log v0.0.36..HEAD --oneline",
+    "requireCleanWorkingDir": false,
+    "commit": true,
+    "commitMessage": "Release ${version}",
+    "tag": true,
+    "tagAnnotation": "Release ${version}",
+    "push": true,
+    "pushArgs": ["--follow-tags"]
+  },
+  "github": {
+    "release": true,
+    "releaseName": "Release ${version}"
+  },
+  "npm": {
+    "publish": false
+  },
+  "gitlab": {
+    "release": false
+  }
+}
--- release.js
+++ release.js
@@ -16,52 +16,54 @@ const exec = util.promisify(require("child_process").exec);
 
 const [from, to] = process.argv.slice(2);
 
+console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
 console.log(process.argv.slice(2));
+console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
 
-const config = conventionalcommits({
-  issuePrefixes: ["TEST-"],
-  issueUrlFormat: "myBugTracker.com/{prefix}{id}",
-  types: [
-    { type: "feat", section: "Features" },
-    { type: "fix", section: "Bug Fixes" },
-    { type: "chore", section: "Chores" },
-    { type: "docs", section: "Docos" },
-    { type: "style", section: "Code Style" },
-    { type: "refactor", section: "Refactors" },
-    { type: "perf", section: "Performance" },
-    { type: "test", section: "Tests" },
-  ],
-});
+// const config = conventionalcommits({
+//   issuePrefixes: ["TEST-"],
+//   issueUrlFormat: "myBugTracker.com/{prefix}{id}",
+//   types: [
+//     { type: "feat", section: "Features" },
+//     { type: "fix", section: "Bug Fixes" },
+//     { type: "chore", section: "Chores" },
+//     { type: "docs", section: "Docos" },
+//     { type: "style", section: "Code Style" },
+//     { type: "refactor", section: "Refactors" },
+//     { type: "perf", section: "Performance" },
+//     { type: "test", section: "Tests" },
+//   ],
+// });
 
-config.then((c) => {
-  conventionalChangelog(
-    {
-      // pkg: {
-      //   transform: function (commit) {
-      //     console.log(arguments);
-      //   },
-      // },
-      releaseCount: 2,
-      config: c,
-    },
-    null,
-    // {
-    //   version: to,
-    // },
-    null,
-    // { from: from },
-    null,
-    {
-      groupBy: "scope",
-      // transform: function (commit, cb) {
-      //   console.log(commit);
-      //   // return;
-      //   // cb();
-      // },
-      // finalizeContext(context, options, commits, keyCommit) {
-      //   // console.log(commits);
-      //   // return { commitGroups: commits };
-      // },
-    }
-  ).pipe(process.stdout);
-});
+// config.then((c) => {
+//   conventionalChangelog(
+//     {
+//       // pkg: {
+//       //   transform: function (commit) {
+//       //     console.log(arguments);
+//       //   },
+//       // },
+//       releaseCount: 2,
+//       config: c,
+//     },
+//     null,
+//     // {
+//     //   version: to,
+//     // },
+//     null,
+//     // { from: from },
+//     null,
+//     {
+//       groupBy: "scope",
+//       // transform: function (commit, cb) {
+//       //   console.log(commit);
+//       //   // return;
+//       //   // cb();
+//       // },
+//       // finalizeContext(context, options, commits, keyCommit) {
+//       //   // console.log(commits);
+//       //   // return { commitGroups: commits };
+//       // },
+//     }
+//   ).pipe(process.stdout);
+// });
