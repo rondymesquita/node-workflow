7b0ff073f841382a7d6378d09367a7700a4ae9f4
rondymesquita <rondinellimesquitas@gmail.com>
2021-08-18 14:10:58 -0300

    chore: remove unused files

--- conv.js
+++
@@ -1,6 +0,0 @@
-const config = require("conventional-changelog-conventionalcommits");
-
-module.exports = config({
-  issuePrefixes: ["TEST-"],
-  issueUrlFormat: "myBugTracker.com/{prefix}{id}",
-});
