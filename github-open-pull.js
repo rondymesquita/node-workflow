ab09c788dc5d650da4a31e633f6edc2e2b243cee
rondymesquita <rondinellimesquitas@gmail.com>
2021-08-23 14:54:09 -0300

    release: 0.0.41

--- github-open-pull.js
+++ github-open-pull.js
@@ -4,6 +4,8 @@ const [name, version, repository ] = args
 const changelog = args.slice(2).join()
 
 console.log('>>>>>>>>>>>>>>>>>>>>>>>>')
-console.log('>>>args', args);
+console.log('>>log args', args);
+console.debug('>>>debug args', args);
+console.info('>>>info args', args);
 console.log('>>>changelog', changelog);
 console.log('>>>>>>>>>>>>>>>>>>>>>>>>')
