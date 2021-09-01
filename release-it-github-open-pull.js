ba4016bf3d2d6c8443e0cf4c7799c37afc6ba26f
rondymesquita <rondinellimesquitas@gmail.com>
2021-09-01 09:41:57 -0300

    chore: get branch name on creating pr

--- release-it-github-open-pull.js
+++ release-it-github-open-pull.js
@@ -1,46 +1,71 @@
-const { Plugin } = require("release-it");
-const { Octokit, App, Action } = require("octokit");
+const { Plugin } = require('release-it')
+const { Octokit, App, Action } = require('octokit')
 
 class MyPlugin extends Plugin {
-  init() {
-    // console.log("init", this);
+  init () {
     // this.octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
   }
-  async beforeRelease() {
-    const context = this.config.getContext();
-    console.log("beforeRelease", context);
-    const { version, changelog } = context;
-    const { pullTitle } = this.options;
-    console.log("beforeRelease", pullTitle);
-    // console.log("beforeRelease", Object.keys(this.config.getContext()));
-    // console.log("beforeRelease", context.changelog);
 
+  async _createPull ({
+    pullTitle,
+    version,
+    tagName,
+    name,
+    repo,
+    changelog
+  }) {
+    const currentBranchName = await this.exec('git branch --show-current', { options: { write: false } });
+    const body = {
+      owner: repo.owner,
+      repo: repo.project,
+      head: currentBranchName,
+      base: 'development',
+      body: changelog,
+      title: 'Release: ' + tagName
+    }
+
+    this.log.verbose('Creating PR with options %o', body)
     try {
-      const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+      const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
       const response = await octokit.request(
-        "POST /repos/rondymesquita/workflow-node/pulls",
-        {
-          owner: "rondymesquita",
-          repo: "workflow-node",
-          head: "dev",
-          base: "main",
-          body: context.changelog,
-          title: "Release: v" + version,
-        }
-      );
-      console.log(response.status);
+        'POST /repos/rondymesquita/workflow-node/pulls', body
+      )
+      this.log.log('PR created', response.data, response.status)
     } catch (err) {
-      console.log(">>>err", err.status);
-      console.log(">>>err", err.response.data);
-      console.log(">>>responerrse", err.response.data.errors);
+      this.log.error('Error when creating Pull')
+      this.log.error('Status: ', err.status)
+      this.log.error('Response: ', err.response.data)
+      this.log.error('Errors: ', err.response.data.errors)
     }
   }
-  // release() {
-  //   console.log("release", this.config.getContext());
-  // }
-  // afterRelease() {
-  //   console.log("afterRelease", this.config.getContext());
-  // }
+
+  async afterRelease () {
+    const context = this.config.getContext()
+    console.log('beforeRelease2', context)
+    const { version, changelog, repo, name, tagName } = context
+    const { pullTitle } = this.options
+    console.log('beforeRelease3', {
+      pullTitle,
+      version,
+      tagName,
+      name,
+      repo,
+      changelog
+    })
+
+    const isDryRun = context['dry-run']
+    if (!isDryRun) {
+      await this._createPull({
+        changelog,
+        name,
+        tagName,
+        repo
+      })
+    } else {
+      this.log.verbose('Running in dry-run mode. No PR will be created')
+
+    }
+  }
 }
 
-module.exports = MyPlugin;
+module.exports = MyPlugin
